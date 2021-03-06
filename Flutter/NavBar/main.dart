f79b5c21b647b983f77bd200a72e6ade5c325a95
Dareka <42326650+DarekaSama@users.noreply.github.com>
2021-03-06 15:38:54 +0100

    Update main.dart

--- Flutter/NavBar/main.dart
+++ Flutter/NavBar/main.dart
@@ -8,6 +8,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'NavBar',
       theme: ThemeData(
         primarySwatch: Colors.blue,
